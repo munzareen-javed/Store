16d786c9cb7d065d897ae533557553a78851438b
abubakarmalik <abubakarzoomii@gmail.com>
2020-03-23 15:51:59 +0500

    Abubakar commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+//dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: `mongodb+srv://abubakar_malik:node.js.es6@cluster0-ujxkl.mongodb.net/store`
+}
