19233703f0d9882b87c4b4f8986532c5e72d80f3
AreejArshad <areejansari49@gmail.com>
2020-04-01 21:57:33 +0500

    1 moudule added
    Commit by Areej Arshad

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const url = require('url');
+const nodemailer = require('nodemailer');
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 // Welcome Page mean main page of website will show
@@ -9,6 +11,19 @@ router.get('/login', forwardAuthenticated, (req, res) => res.render('login'));
 
 //for Register page
 router.get('/register', forwardAuthenticated, (req, res) => res.render('register'));
+
+//for contact page
+router.get('/contact', forwardAuthenticated, (req, res) => res.render('contact'));
+//for contact success
+router.get('/contactSuccess', (req, res) => {
+  console.log('Request for contact send page recieved');
+  res.render('contactSuccess');
+});
+//for contact failed
+router.get('/contactError', (req, res) => {
+  console.log('Request for contact error page recieved');
+  res.render('contactError');
+});
 
 
 //for contact page
